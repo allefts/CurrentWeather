532bdcf1dd3d0f3ba34817c3dc3c8b29e8bbcc35
Allef Soares <alleftsoares@gmai.com>
2020-10-23 21:53:19 -0700

    done!
    Reads off the openweather API for the current weather in Austin.
    
    Displays it in a semi-cool one page app with icons changing depending on the temperature.

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 import { FiSun } from "react-icons/fi";
+import { FaSnowflake } from "react-icons/fa";
 import { IoIosRainy } from "react-icons/io";
 import { TiWeatherCloudy } from "react-icons/ti";
+import { RiTempColdFill } from "react-icons/ri";
 
 class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.icons = [
+      <FiSun />,
+      <IoIosRainy />,
+      <TiWeatherCloudy />,
+      <FaSnowflake />,
+      <RiTempColdFill />,
+    ];
+  }
   state = {
     weather: null,
     temp: null,
@@ -32,18 +44,33 @@ class Weather extends React.Component {
           description: res.data.weather[0].description, //Description
           location: res.data.name, //Place
         });
+        this.checkIcon();
       });
   }
 
+  checkIcon = () => {
+    const currentTemp = this.state.temp;
+    if (currentTemp < 50) {
+      this.setState({ icon: this.icons[3] });
+    } else if (currentTemp < 50 && !currentTemp < 60) {
+      this.setState({ icon: this.icons[4] });
+    } else if (currentTemp < 60) {
+      this.setState({ icon: this.icons[2] });
+    } else if (currentTemp < 70) {
+      this.setState({ icon: this.icons[1] });
+    } else {
+      this.setState({ icon: this.icons[0] });
+    }
+  };
+
   render() {
     const degrees = <span style={{ marginLeft: ".25rem" }}>&#176;F</span>;
-    const icons = [<FiSun />, <IoIosRainy />, <TiWeatherCloudy />];
-
     return (
       <div>
         <div className="informationWrapper">
           <h2 className="location">{this.state.location}, TX</h2>
           <div className="line"></div>
+          <div className="iconWrapper">{this.state.icon}</div>
           <div className="informationContainer">
             <div className="tempWrapper">
               <h1 className="temp">
